5dc79ec1f6d61c3304a9e8e6a4cbaea3efd5fcfc
jchelmsMedia <j.helms@helmsmedia.net>
2024-01-19 17:56:53 -0500

    initial commit
    Initial commit

---
+++ data.js
@@ -0,0 +1,53 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-_dsc6046-panorama-reconstructed",
+      "name": "_DSC6046 Panorama Reconstructed",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2612,
+      "initialViewParameters": {
+        "yaw": 0.04349243298512917,
+        "pitch": 0.053429481164968706,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.4654526566228867,
+          "pitch": -0.042326199474757686,
+          "title": "Home",
+          "text": "Home Information"
+        }
+      ]
+    }
+  ],
+  "name": "Test Panorama",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
